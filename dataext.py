3af0f534dd946d6a43be9830fa0d08e3c5550eaa
LucasSpektralInc <lucas.amorimsegantin.885@accesscreative.ac.uk>
2023-09-21 13:59:05 +0100

    Whole code is gone

--- dataext.py
+++ dataext.py
@@ -3,53 +3,24 @@ import turtle
 def test():
      print("Yeah that works!")
 
-
-class Person:
-
-    def __init__(self,  name, age, hair, sex, role):
-        self.name = name
-        self.age = age
-        self.hair = hair
-        self.sex = sex
-        self.role = role
-
 class DataTable:
      
      def __init__(self):
-          self.data = []
-          
+          self.data = {}
 
-     def AddPerson(self, name, age, sex, role, hair_colour):
-        n = Person(name, age, hair_colour, sex, role)
-        self.data.append(n)
+     def insert_data(self, name, **kwargs):
+         self.data[name] = kwargs
+         self.data[name]["Name"] = name
 
-     def CountHairColour(self, colour):
-        c = 0
-        for i in self.data:
-            if i.hair == colour:
-                c += 1
-        return c
-     
-     def CountSex(self, sex):
-        c = 0
-        for i in self.data:
-            if i.sex == sex:
-                c += 1
-        return c
-     
-     def CountRole(self, role):
+     def count_data(self, data, value):
          c = 0
          for i in self.data:
-             if i.role == role:
+             if self.data[i][data] == value:
                  c += 1
          return c
-     
-
+      
 if __name__ == "__main__":
     test = DataTable()
-    test.AddPerson("Lucas", 16, "Male", "Musician", "Brown")
-    test.AddPerson("Maja", 17, "Female", "Programmer", "Brown")
-    test.AddPerson("Micaiah", 18, "Male", "Artist", "Black")
-    test.AddPerson("Fillip", 16, "Male", "Musician", "Blonde")
-    test.AddPerson("Joao", 16, "Male", "Programmer", "Ginger")
-    print(test.CountRole("Musician"))
+    test.insert_data("Joao", Name = "Joao", Age = 16, County = "London", Sex = "Male")
+    test.insert_data("Maja", Name = "Maja", Age = 17, County = "London", Sex = "Female")
+    print(test.count_data("County", "London"))
