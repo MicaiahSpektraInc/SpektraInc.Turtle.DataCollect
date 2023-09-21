2f5d3ec582299941ae3fc9578f291e65aac6b98d
LucasSpektralInc <lucas.amorimsegantin.885@accesscreative.ac.uk>
2023-09-21 12:02:30 +0100

    Added Counters

--- dataext.py
+++ dataext.py
@@ -1,54 +1,49 @@
 import turtle
 
-class Personal:
+def test():
+     print("Yeah that works!")
 
-    def __init__(self,  name, age,):
+
+class Person:
+
+    def __init__(self,  name, age, hair, sex, role):
         self.name = name
         self.age = age
-
-p1 = Personal[name1],  age[1],
-              name[2],  age[2],
-              name[3],  age[3],
-              name[4],  age[4],
-              name[5],  age[5],
-              name[6],  age[6],
-              name[7],  age[7],
-              name[8],  age[8],
-              name[9],  age[9],
-              name[10],  age[10],
-              name[11],  age[11],
-              name[12],  age[12],
-              name[13],  age[13],
-              name[14],  age[14]
-
-class Appearance:
-    def __init__(self, hair_colour, sex,role):
-        self.hair_colour = hair_colour
+        self.hair = hair
         self.sex = sex
-
-a1 = Appearance[hair_colour1, sex1,
-                hair_colour[2],  sex[2],
-                hair_colour[3],  sex[3],
-                hair_colour[4],  sex[4],
-                hair_colour[5],  sex[5],
-                hair_colour[6],  sex[6],
-                hair_colour[7],  sex[7],
-                hair_colour[8],  sex[8],
-                hair_colour[9],  sex[9],
-                hair_colour[10],  sex[10],
-                hair_colour[11],  sex[11],
-                hair_colour[12],  sex[12],
-                hair_colour[13],  sex[13],
-                hair_colour[14],  sex[14],]
-
-class Job:
-    def __init__(self, role):
         self.role = role
 
-j1 = Job[role[1], role[2],
-        role[3],  role[4],
-        role[5],  role[6],
-        role[7],  role[8],
-        role[9],  role[10],
-        role[11],  role[12],
-        role[13],  role[14]]
+class DataTable:
+     
+     def __init__(self):
+          self.data = []
+          
+
+     def AddPerson(self, name, age, sex, role, hair_colour):
+        n = Person(name, age, hair_colour, sex, role)
+        self.data.append(n)
+
+     def CountHairColour(self, colour):
+        c = 0
+        for i in self.data:
+            if i.hair == colour:
+                c += 1
+        return c
+     
+     def CountSex(self, sex):
+        c = 0
+        for i in self.data:
+            if i.sex == sex:
+                c += 1
+        return c
+     
+     def CountRole(self, role):
+         c = 0
+         for i in self.data:
+             if i.role == role:
+                 c += 1
+         return c
+
+test = DataTable()
+test.AddPerson("Lucas", 16, "Male", "Student", "Brown")
+print(test.CountSex("Male"))
