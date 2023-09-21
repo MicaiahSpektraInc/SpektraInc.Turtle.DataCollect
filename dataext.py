9526eff0e4afc1daa160f22fbc4940d8e1ce9978
Filip Djukic <filip.djukic.912@accesscreative.ac.uk>
2023-09-21 10:20:57 +0100

    added data backend

---
+++ dataext.py
@@ -0,0 +1,51 @@
+import turtle
+
+class Personal:
+
+    def __init__(self,  name, age,):
+        self.name = name
+        self.age = age
+
+p1 = Personal(list[name1,  age1,
+              name2,  age2,
+              name3,  age3,
+              name4,  age4,
+              name5,  age5,
+              name6,  age6,
+              name7,  age7,
+              name8,  age8,
+              name9,  age9,
+              name10,  age10,
+              name11,  age11,
+              name12,  age12,
+              name13,  age13,
+              name14,  age14])
+
+class Appearnce:
+    def __init__(self, hair_colour, sex,role):
+        self.hair_colour = hair_colour
+        self.sex = sex
+
+a1 = Appearance(list[hair_colour1, sex1,
+                hair_colour2,  sex2,
+                hair_colour3,  sex3,
+                hair_colour4,  sex4,
+                hair_colour5,  sex5,
+                hair_colour6,  sex6,
+                hair_colour7,  sex7,
+                hair_colour8,  sex8,
+                hair_colour9,  sex9,
+                hair_colour10,  sex10,
+                hair_colour11,  sex11,
+                hair_colour12,  sex12,
+                hair_colour13,  sex13,
+                hair_colour14,  sex14,])
+count  = 
+
+
+
+class Job:
+    def __init__(self, role):
+        self.role = role
+
+j1 = Job(role1, role2,  role3,  role4,  role5,  role6,  role7,  role8,  role9,  role10,  role11,  role12,  role13,  role14)
